e10ed7427efb8ac7a062a5fcc9e62d9db5bd71f3
muzaffarhaque <muzaffarhaque777@gmail.com>
2024-08-31 20:47:00 +0530

    Subject : hero added

--- src/app/(pages)/academy-list/page.tsx
+++ src/app/(pages)/academy-list/page.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function AcademyList() {
   return (
-    <div>page</div>
+    <section className='academy-main-page'>
+    <div className="hero-section-wrapper">
+      <h1 className='large'>Pure & mindful elegance</h1>
+      <p className='b-e-lg'>BlueHans offers premium UX/UI design, advanced design training, custom AI solutions. </p>
+    </div>
+  </section>
   )
 }
